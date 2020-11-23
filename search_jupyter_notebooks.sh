fa317f83613c5b07c2be6db3505a03442050a542
Young-Chan Park <young.chan.park93@gmail.com>
2020-11-23 01:10:18 +0000

    Updated search_jupyter_notebooks.sh

--- search_jupyter_notebooks.sh
+++ search_jupyter_notebooks.sh
@@ -1,6 +1,13 @@
-find notebooks_dir -type f -name "*.ipynb" ! -name "*checkpoint*" |
+#!/usr/bin/env bash
+notebook_dir=$1
+search=$2
+
+find $notebook_dir -type f -name "*.ipynb" ! -name "*checkpoint*" |
 while read f
 do
-	echo $f
-	grep ".sum" "$f" 
-done | less
+    output=$(grep "$search" "$f")
+    if [[ -n "$output" ]]
+    then
+        echo $f
+    fi
+done
